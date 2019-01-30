7905ab074fc7fd57fd464fb404329f46fed68ecb
Karen HY Tu <karenhaoyitu@gmail.com>
2019-01-30 12:08:35 +0800

    Lack of Parameter

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, My Server'))
 
+
+//Build Simple RESTful API 
+app.get('/getData', function (req, res) {
+  res.send('Lack of Parameter')
+})
+app.get('/getData?number=xyz​', function (req, res) {
+  res.send('Wrong Parameter')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
